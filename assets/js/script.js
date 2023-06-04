888b3317c4e87be92959dde2a5213962240e24fd
zain srio <zein.srio@gmail.com>
2023-06-05 00:57:31 +0200

    added a slider, section of services and a video

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+$( function() {
+  $( "#inputCheckIn" ).datepicker();
+  $( "#inputCheckOut" ).datepicker();
+
+
+
+  $('.level-slider').slick({
+    dots:true,
+    slidesToShow: 2,
+    arrows:true,
+    slidesToScroll: 2,
+  });
+
+
+} );
